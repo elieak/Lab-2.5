3ce6a0f7e88d09a71da22cf6a1441922293f5c78
Elie <Elie@CODEVALUE>
2016-06-23 10:59:54 +0300

    aa

---
+++ MulBoard/MulBoard/MulBoard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MulBoard
+{
+    public class MulBoard
+    {
+        public static void Main()
+        {
+            var i = 1;
+            while (i <= 10)
+            {
+                var j = 1;
+                var line = "";
+                while (j <= 10)
+                {
+                    line += (i*j).ToString() + "\t";
+                    j++;
+                }
+                Console.WriteLine(line);
+                i++;
+
+            }
+        }
+    }
+}
